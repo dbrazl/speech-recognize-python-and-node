a22ca31618dba7943e6266b87e6cbe124870b454
dbrazl <danielbrazlopes16@gmail.com>
2020-02-20 22:30:16 -0300

    remove comentaries

--- speechRecognize.py
+++ speechRecognize.py
@@ -1,25 +1,18 @@
 import speech_recognition as sr
-    #Funcao responsavel por ouvir e reconhecer a fala
-def ouvir_microfone():
-#Habilita o microfone para ouvir o usuario
-    microfone = sr.Recognizer()
-    with sr.Microphone() as source:
-    #Chama a funcao de reducao de ruido disponivel na speech_recognition
-        microfone.adjust_for_ambient_noise(source)
-        #Avisa ao usuario que esta pronto para ouvir
-        print("Diga alguma coisa: ")
-        #Armazena a informacao de audio na variavel
-        audio = microfone.listen(source)
 
+def listenMicrophone():
+
+    microphone = sr.Recognizer()
+    with sr.Microphone() as source:
+        microphone.adjust_for_ambient_noise(source)
+        print("Diga alguma coisa: ")
+        audio = microphone.listen(source)
         print("Processando...")
     try:
-    #Passa o audio para o reconhecedor de padroes do speech_recognition
-        frase = microfone.recognize_google(audio,language='pt-BR')
-        #Após alguns segundos, retorna a frase falada
-        print("Você disse: " + frase)
-        #Caso nao tenha reconhecido o padrao de fala, exibe esta mensagem
+        phrase = microphone.recognize_google(audio,language='pt-BR')
+        print("Você disse: " + phrase)
     except sr.UnkownValueError:
         print("Não entendi")
-        return frase
+        return phrase
 
-ouvir_microfone()
+listenMicrophone()
